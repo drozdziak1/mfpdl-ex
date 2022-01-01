6808c6956881c90d213d1bb37e0601a383f00050
Stan Drozd <drozdziak1@gmail.com>
2022-01-02 00:05:44 +0100

    Ignore /about and /credits

--- lib/learning.ex
+++ lib/learning.ex
@@ -16,11 +16,18 @@ defmodule Learning do
     {:ok, _status, _headers, client} = :hackney.request(@mfp_url <> "latest")
     {:ok, body} = :hackney.body(client)
     {:ok, html} = Floki.parse_document(body)
-    [_rss1, _rss2 | links] = Floki.find(html, "#sapper a")
-    links
+    # Remove RSS links
+    [_rss1, _rss2 | links]= Floki.find(html, "#sapper a")
+    # Remove /about and /credits
+    links = links |> Enum.reverse() |> tl() |> tl()
+    Enum.map(links, fn entry ->
+      {_a, [{"href", link}], _contents} = entry
+      @mfp_url <> link
+    end)
   end
 
   def get_mp3_link(url) do
+    IO.puts("Processing url #{url}")
     {:ok, _status, _headers, client} = :hackney.request(url)
     {:ok, body} = :hackney.body(client)
     {:ok, html} = Floki.parse_document(body)
@@ -32,6 +39,7 @@ defmodule Learning do
 
   def get_all_mp3_links do
     links = find_ep_page_links()
-
+    s = Task.async_stream(links, fn l -> get_mp3_link(l) end, [])
+    Enum.reduce(s, [], fn {:ok, link}, acc -> acc ++ [link] end)
   end
 end
